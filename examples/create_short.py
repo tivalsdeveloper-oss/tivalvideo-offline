4ce447f7c332559421b3e8be5ccf290416c97d2b
Tivalsdeveloper <tivalsdeveloper@gmail.com>
2026-08-29 15:09:03 +0200

    Add examples/create_short.py

---
+++ examples/create_short.py
@@ -0,0 +1,7 @@
+from tivalvideo import create_video
+
+images = ["lesson-1.png", "lesson-2.png", "lesson-3.png"]
+narration = "Learn TivalTube step by step. Powered by Tivalsdeveloper."
+
+create_video(images, narration, "my-short.mp4")
+
