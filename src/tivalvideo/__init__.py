d97170b3b22339f4a889672d697f985344a64170
Tivalsdeveloper <tivalsdeveloper@gmail.com>
2026-08-30 13:03:58 +0200

    Add selectable narrator voices to src/tivalvideo/__init__.py

--- src/tivalvideo/__init__.py
+++ src/tivalvideo/__init__.py
@@ -1,7 +1,10 @@
 """Public API for TivalVideo Offline."""
 
 from .maker import VideoConfig, VideoError, VideoMaker, create_video
-from .tools import ToolManager
+from .tools import DEFAULT_VOICE, VOICE_CATALOG, ToolManager, VoiceSpec
 
-__all__ = ["ToolManager", "VideoConfig", "VideoError", "VideoMaker", "create_video"]
-__version__ = "0.2.0"
+__all__ = [
+    "DEFAULT_VOICE", "VOICE_CATALOG", "ToolManager", "VideoConfig", "VideoError",
+    "VideoMaker", "VoiceSpec", "create_video",
+]
+__version__ = "0.3.0"
