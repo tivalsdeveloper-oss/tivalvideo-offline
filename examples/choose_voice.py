b9dfb31bf3ab3f991830f2c5e2220bfadd3235f6
Tivalsdeveloper <tivalsdeveloper@gmail.com>
2026-08-30 13:04:10 +0200

    Add selectable narrator example

---
+++ examples/choose_voice.py
@@ -0,0 +1,9 @@
+from tivalvideo import create_video
+
+create_video(
+    typing_text="The History of Python",
+    narration="Python was created by Guido van Rossum.",
+    voice="en_GB-alan-medium",
+    background="midnight",
+    output="python-history.mp4",
+)
